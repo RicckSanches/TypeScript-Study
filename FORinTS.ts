dd3d607b5e16a6c4d6815bb71d6a8c1ebbeb48fe
RicckSanches <failkahusnutdinov@gmail.com>
2025-07-23 22:49:21 +0300

    4, 5 lesson

---
+++ FORinTS.ts
@@ -0,0 +1,17 @@
+//Rewrite the following code using TypeScript:
+/*
+let res = 0;
+
+for (let i = 0; i < 100; i++) {
+	res += i;
+}
+
+console.log(res);
+ */
+
+let result: number = 0;
+
+for (let i: number = 0; i < 100; i++) {
+    result+= i;
+}
+console.log(result);
