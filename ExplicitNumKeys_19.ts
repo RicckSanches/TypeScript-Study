be5589ff3c480935b2f0e05980dbf7d2be9b017b
RicckSanches <failkahusnutdinov@gmail.com>
2025-07-24 05:29:55 +0300

    22 lesson

--- ExplicitNumKeys_19.ts
+++ ExplicitNumKeys_19.ts
@@ -10,8 +10,9 @@ console.log(current); // выведет 1
 
  */
 
-enum Months { January = 1, February = 2, March = 3, April = 4, May = 5 , June = 6, Jule = 7, August = 8, September = 9, October = 10, November=11, December = 12 };
 
-let currentMonth1: Months = Months.January;
-let currentMonth5: Months = Months.May;
+enum Months1 { January = 1, February = 2, March = 3, April = 4, May1 = 5 , June = 6, Jule = 7, August = 8, September = 9, October = 10, November=11, December = 12 };
+
+let currentMonth1: Months1 = Months1.January;
+let currentMonth5: Months1 = Months1.May1;
 console.log(currentMonth1, currentMonth5);
